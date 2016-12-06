e4688cd14d6d19d842d8d8e59a8ac22f8c902343
zcc <q821386496@qq.com>
2016-12-06 16:59:24 +0800

    创建sinaWeibo

---
+++ sinaWeibo/sinaWeibo/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  sinaWeibo
+//
+//  Created by qx-005 on 16/12/6.
+//  Copyright © 2016年 zcc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
