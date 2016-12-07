bdb4d9910b74438921f8f60ab4789944ef94cde4
zcc <q821386496@qq.com>
2016-12-07 11:49:05 +0800

    第一天第一个上午

---
+++ sinaWeibo/sinaWeibo/ZCCRandomColor.h
@@ -0,0 +1,12 @@
+//
+//  ZCCRandomColor.h
+//  sinaWeibo
+//
+//  Created by qx-005 on 16/12/7.
+//  Copyright © 2016年 zcc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#define ZCCRCOLOR [UIColor colorWithRed:arc4random_uniform(256)/255.0 green:arc4random_uniform(256)/255.0 blue:arc4random_uniform(256)/255.0 alpha:1.0]
+
